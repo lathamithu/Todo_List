f870ccb6a682c5f6bf3f99673e323a34a5ed1480
Meghana <53141586+lathamithu@users.noreply.github.com>
2020-03-18 22:34:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Navbar from './components/navbar';
 import Back from './components/back';
+
  function App () {
     return (
-        <div>
+        <>
             <Navbar/>
-            <Back/>
-        </div>
+                <div id="sec"> 
+                    <div id="sec2">
+                        <div id="sec3">
+                        <Back/>
+                            <div id="sec4">
+                            <ul id="lst">
+                            </ul>
+                            </div>
+                         </div>
+                    </div>
+                 </div>
+        </>
     );
 }
 
